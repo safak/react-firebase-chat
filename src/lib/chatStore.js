939e2eef45947d586f4e3662c0d029f923558bae
Safak <1486366+safak@users.noreply.github.com>
2024-04-10 16:59:17 +0200

    add reset action

--- src/lib/chatStore.js
+++ src/lib/chatStore.js
@@ -40,4 +40,12 @@ export const useChatStore = create((set) => ({
   changeBlock: () => {
     set((state) => ({ ...state, isReceiverBlocked: !state.isReceiverBlocked }));
   },
+  resetChat: () => {
+    set({
+      chatId: null,
+      user: null,
+      isCurrentUserBlocked: false,
+      isReceiverBlocked: false,
+    });
+  },
 }));
