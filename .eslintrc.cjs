268d1b6d24dd8f32fd2201a60796161627944482
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2024-04-16 19:56:41 +0200

    emojipicker library

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,22 +2,22 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
     "react/prop-types": "off",
     "no-unused-vars": "warn",
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
   },
-}
+};
--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,59 @@
+import { useState } from "react";
 import "./chat.css";
+import EmojiPicker from "emoji-picker-react";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmoji = (e) => {
+    setText((prev) => prev + e.emoji);
+  };
+
+  console.log(text);
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Francesca Navas</span>
+            <p>Papa ti amo!</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <div className="icons">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          type="text"
+          placeholder="Type a message.."
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picket">
+            <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+          </div>
+        </div>
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
