42fb58fa238212fc5db52da2ec8c1bd34cbb6c9c
Irmina Kalinowska <irminakalinowska0@gmail.com>
2024-05-16 07:43:03 +0200

    adding a user

--- src/components/list/chatList/addUser/AddUser.jsx
+++ src/components/list/chatList/addUser/AddUser.jsx
@@ -1,9 +1,32 @@
 import './addUser.css';
+import { db } from '../../../../lib/firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { useState } from 'react';
 
 const AddUser = () => {
+    const [user, setUser] = useState(null);
+    const handleSearch = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const username = formData.get('username');
+
+        try {
+            const userRef = collection(db, 'users');
+
+            const q = query(userRef, where('username', '==', username));
+
+            const querySnapShot = await getDocs(q);
+
+            if (!querySnapShot.empty) {
+                setUser(querySnapShot.docs[0].data());
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    };
     return (
         <div className='addUser'>
-            <form>
+            <form onSubmit={handleSearch}>
                 <input
                     type='text'
                     placeholder='Username'
@@ -11,16 +34,18 @@ const AddUser = () => {
                 />
                 <button>Search</button>
             </form>
-            <div className='user'>
-                <div className='detail'>
-                    <img
-                        src='./avatar.png'
-                        alt=''
-                    />
-                    <span>Jane Doe</span>
+            {user && (
+                <div className='user'>
+                    <div className='detail'>
+                        <img
+                            src={user.avatar || '/avatar.png'}
+                            alt=''
+                        />
+                        <span>{user.username}</span>
+                    </div>
+                    <button>Add User</button>
                 </div>
-                <button>Add User</button>
-            </div>
+            )}
         </div>
     );
 };
