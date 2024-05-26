18800640fc396d1373ee927b286b7cf144d416d9
Eleaxgaymer138 <158332678+Eleaxgaymer138@users.noreply.github.com>
2024-05-25 22:29:33 -0600

    Update Login.jsx

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -33,12 +33,11 @@ const Login = () => {
 
     const { username, email, password } = Object.fromEntries(formData);
 
-    // VALIDATE INPUTS
     if (!username || !email || !password)
       return toast.warn("Please enter inputs!");
     if (!avatar.file) return toast.warn("Please upload an avatar!");
 
-    // VALIDATE UNIQUE USERNAME
+
     const usersRef = collection(db, "users");
     const q = query(usersRef, where("username", "==", username));
     const querySnapshot = await getDocs(q);
