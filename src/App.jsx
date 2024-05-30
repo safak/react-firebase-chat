67c8740d3e9f4f86bf06c1d8de0febac08e0a479
SergeBoucka <serge.boucka@gmail.com>
2024-05-30 21:15:54 +0200

    react management tools

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,26 @@ import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { auth } from "./lib/firebase";
 
 
 const App = () => {
   const user = true;
+
+useEffect(() => {
+  const unSub = onAuthStateChanged(auth, (user) => {
+    console.log(user);
+  });
+
+  return () => {
+    unSub();
+  };
+} , []);
+
+
+
 
   return (
     <div className="container">
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,24 +1,83 @@
-import { toast } from "react-toastify";
-import "./login.css";
 import { useState } from "react";
+import "./login.css";
+import { toast } from "react-toastify";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../../lib/firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { upload } from "../../lib/upload";
+import { set } from "firebase/database";
 
 const Login = () => {
-const [avatar, setAvatar] = useState({
+  const [avatar, setAvatar] = useState({
     File: null,
     URL: "",
-});
+  });
 
-const handleAvatar = (e) => { 
+  const [loading, setLoading] = useState(false);
+
+  const handleAvatar = (e) => {
     if (e.target.files[0]) {
-    setAvatar({
+      setAvatar({
         File: e.target.files[0],
         URL: URL.createObjectURL(e.target.files[0]),
-    });
-    }}
+      });
+    }
+  };
 
-const handleLogin = (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
-}
+    setLoading(true);
+    const formData = new FormData(e.target);
+
+    const { username, email, password } = Object.fromEntries(formData);
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const imgUrl = await upload(avatar.File);
+
+      await setDoc(doc(db, "users", res.user.uid), {
+        username,
+        email,
+        avatar: imgUrl,
+        id: res.user.uid,
+        blocked: [],
+      });
+
+      await setDoc(doc(db, "userchats", res.user.uid), {
+        chats: [],
+      });
+
+      toast.success(
+        "Bienvenue sur Black Cupidon, vous pouvez maintenant vous connecter."
+      );
+    } catch (err) {
+      console.error(err);
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+setLoading(true);
+
+const formData = new FormData(e.target);
+
+    const {email, password } = Object.fromEntries(formData);
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+      toast.error(err.message);
+      
+    }
+finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="login">
@@ -27,21 +86,29 @@ const handleLogin = (e) => {
         <form onSubmit={handleLogin}>
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
-          <button>Se connecter</button>
+          <button disabled={loading}>{loading ? "Loading" : "Se connecter" }</button>
         </form>
       </div>
       <div className="separator"></div>
       <div className="item">
         <h2>Creer un compte</h2>
-        <form>
+        <form onSubmit={handleRegister}>
           <label htmlFor="file">
             <img src={avatar.URL || "./avatar.png"} alt="" />
-            Télécharger une image</label>
-          <input type="file" id="file" style={{ display: "none" }} onChange={handleAvatar}/>
+            Télécharger une image
+          </label>
+          <input
+            type="file"
+            id="file"
+            style={{ display: "none" }}
+            onChange={handleAvatar}
+          />
           <input type="text" placeholder="Username" name="username" />
           <input type="text" placeholder="Email" name="email" />
           <input type="password" placeholder="Password" name="password" />
-          <button>Se connecter</button>
+          <button disabled={loading}>
+            {loading ? "Loading" : "S'inscrire"}
+          </button>
         </form>
       </div>
     </div>
