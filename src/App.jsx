7fffda51a37ee643e5376d205be41c334779106c
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-06-06 01:29:17 +0400

    Store User After Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
 import Chat from "./components/chat/Chat";
 import Detail from "./components/detail/Detail";
 import List from "./components/list/List";
 import Login from "./components/login/Login";
 import Notification from "./components/notification/Notification";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./lib/firebase";
 
 const App = () => {
   const user = false;
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      console.log(user);
+    });
+
+    return () => unSub();
+  }, []);
 
   return (
     <div className="flex h-90v w-80v rounded-xl border border-white/10 bg-dark-blue backdrop-blur-lg backdrop-saturate-100">
