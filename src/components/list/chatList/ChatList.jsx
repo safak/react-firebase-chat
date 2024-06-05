f211c18926eb43fc5140f52a67945c1e09abb7d9
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-06-05 04:07:43 +0400

    Search Bar Design

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+
 function ChatList() {
-  return <div>ChatList</div>;
+  const [addMode, setAddMode] = useState(false);
+
+  return (
+    <div className="">
+      <div className="flex items-center gap-5 p-5">
+        <div className="bg-dark-blue flex flex-1 gap-5 rounded-[10px] p-[10px]">
+          <img className="h-5 w-5" src="./search.png" alt="" />
+          <input
+            type="text"
+            placeholder="Search..."
+            className="flex border-none bg-transparent text-white outline-none placeholder:text-sm"
+          />
+        </div>
+        <img
+          className="bg-dark-blue h-9 w-9 cursor-pointer rounded-[10px] p-[10px]"
+          src={addMode ? "./minus.png" : "./plus.png"}
+          onClick={() => setAddMode((prevState) => !prevState)}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ChatList;
