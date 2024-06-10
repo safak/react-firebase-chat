c5522cef895b4c923c1fe665b144dd5397b28b39
Pritam-Fulari <125819433+Pritam-Fulari@users.noreply.github.com>
2024-06-10 13:34:49 +0530

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -14,6 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth()
-export const db = getFirestore()
-export const storage = getStorage()
+export const auth = getAuth(app)
+export const db = getFirestore(app)
+export const storage = getStorage(app)
