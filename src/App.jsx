4650bcaf215683e80301734cfb9edd0aa5c4944e
Abraham <tikuyeabraham@gmail.com>
2024-06-18 02:12:24 +0300

    Added new files

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import React from "react";
 import Chats from "./chat/Chats";
 import List from "./list/list";
 import Detail from "./details/detail";
+import Login from "./login/Login";
 const App = () => {
-  return (
-    <div className='container flex'>
-          <List/>
-          <Chats/>
-           
-           <Detail/>
 
+const user= false;
+
+  return (
+    <div className='container flex '>
+      
+        { user ? ( 
+        <> <List/>
+          <Chats/>
+           <Detail/>
+           </>
+           ) :(<Login/>)}
     </div>
   
   )
---
+++ src/login/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './login.css'
+const Login=()=> {
+  return (
+    <div>Login</div>
+  )
+} 
+
+export default Login
